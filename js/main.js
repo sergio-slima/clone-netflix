95bba877e612f4f6bbf7cc8ec5379a558d1d1240
unknown <sergio_silvalima@hotmail.com>
2021-01-15 07:16:21 -0300

    Projeto completo básico

--- js/main.js
+++ js/main.js
@@ -22,3 +22,27 @@ function boxInfor1() {
         info.style.opacity = 0;
     }
 }
+
+$('.nav a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
+
+$(document).ready(function () {
+    $(".back-to-top").css("display", "block");
+  $("body").scroll(function() {
+      if($(this).scrollTop() == 0){
+        $(".back-to-top").css("display", "none");
+      } else {
+        $(".back-to-top").css("display", "block");
+      }
+    });
+  $(".back-to-top").click(function() {
+      $("html, body").animate({scrollTop: 0}, 800);
+     });
+  });
